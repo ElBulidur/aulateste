616f588d2a309abfb08f648ef475716df9f1a40e
elbulidur <juliocezar.marketing@gmail.com>
2022-05-31 08:48:53 -0300

    primeira aula

---
+++ main.py
@@ -0,0 +1,22 @@
+ # comentario
+
+"""
+sdksdksd;k
+sdksad
+slad
+"""
+
+from unicodedata import decimal
+from sqlalchemy import Integer
+
+
+print("Tipos de variaveis")
+
+# Espaco na memoria para armazenamento de informacoes
+
+inteiro = 20
+print(inteiro)
+print(type(inteiro))
+
+valor_decimal = 0.5
+
